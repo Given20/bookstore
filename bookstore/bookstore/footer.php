468ee718a942e449e4fffb814355bf6251c39f85
Sinhle Gamede <83158180+Given20@users.noreply.github.com>
2023-03-09 19:33:58 +0200

    Add files via upload

---
+++ bookstore/bookstore/footer.php
@@ -0,0 +1,38 @@
+<link rel="stylesheet" href="css/style.css" type="text/css">
+
+<section class="footer">
+
+	<div class="box-container">
+		<div class="box">
+			<h3>quick links</h3>
+				<p><a href="home.php">Home</a></p>
+				<p><a href="about.php">About</a></p>
+				<p><a href="shop.php">Shop</a></p>
+				<p><a href="contact.php">Contact</a></p>
+				<p><a href="orders.php">Orders</a></p>
+		</div>
+		
+		<div class="box">
+			<h3>extra links</h3>
+				<p><a href="Login.php">Login</a></p>
+				<p><a href="register.php">register</a></p>
+				<p><a href="orders.php">orders</a></p>
+		</div>
+		
+		<div class="box">
+			<h3>Contact Us</h3>
+				<p> <img src="Logos/phone.png" alt="contact" width="35" height="35"></a> +27762295434 </p>
+				<p>  <img src="Logos/mail.jpg" alt="email" width="35" height="35"></a>booksRus@gmail.com </p>
+				<p>  <img src="Logos/house.jpg" alt="Address" width="35" height="35"></a>624 Unit A<br>Pretoria<br> 0002</p>
+		</div>
+		
+		<div class="box">
+			<h3>Social Media</h3>
+				<p><a href="#" class="fab fa-instagram"><img src="Logos/insta.jpg" alt="instergram" width="35" height="35">Instergram</a></p>
+				<p><a href="#" class="fab fa-twitter"><img src="Logos/Twitter.png" alt="twitter" width="35" height="35">Twitter</a></p>
+				<p><a href="#" class="fab fa-Youtube"><img src="Logos/youtube.png" alt="youtube" width="35" height="35">Youtube</a></p>
+		</div>
+	</div>
+	
+	<p> created by <span>Books'R'Us</span> @<?php echo date('Y') ?> | all rights reserved!</p>
+</section>
